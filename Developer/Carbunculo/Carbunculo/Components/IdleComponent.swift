9fee190734e1bd40a7144ed01231763dd9d14888
jeffoliveira019 <jefferson.lalor@icomp.ufam.edu.br>
2019-05-21 14:28:26 -0400

    Adicionando componente de idle

---
+++ Developer/Carbunculo/Carbunculo/Components/IdleComponent.swift
@@ -0,0 +1,31 @@
+//
+//  IdleComponent.swift
+//  Carbunculo
+//
+//  Created by Jefferson de Oliveira Lalor on 21/05/19.
+//  Copyright © 2019 CarbunculoTeam. All rights reserved.
+//
+
+import Foundation
+import GameplayKit
+
+
+class IdleComponent: GKComponent {
+    
+    weak var node: PlayerNode!
+    
+    init(player: PlayerNode) {
+        super.init()
+        
+        node = player
+    }
+    
+    func idle() {
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
