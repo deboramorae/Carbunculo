6214f4b8a15810a82c1b2b8fd748bd3e348611f2
Unknown <neto3333@gmail.com>
2019-05-21 13:05:36 -0400

    Player State machine is now created.

---
+++ Developer/Carbunculo/Carbunculo/StateMachines/MachinePlayer.swift
@@ -0,0 +1,18 @@
+//
+//  MachinePlayer.swift
+//  Carbunculo
+//
+//  Created by Ruy de Ascencão Neto on 21/05/19.
+//  Copyright © 2019 CarbunculoTeam. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+class MachinePlayer:GKStateMachine{
+    weak var node:PlayerNode!
+    init(states: [GKState],player:PlayerNode) {
+        node = player
+        super.init(states: states)
+    }
+}
