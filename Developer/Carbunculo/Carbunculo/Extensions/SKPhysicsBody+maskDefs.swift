595b157715f809b3001aea76b0088b8a1644ce88
Débora Moraes <deboramp.lissa@gmail.com>
2019-05-22 13:46:10 -0400

    adicionando extension Physics - categoryBitMask

---
+++ Developer/Carbunculo/Carbunculo/Extensions/SKPhysicsBody+maskDefs.swift
@@ -0,0 +1,18 @@
+//
+//  SKPhysicsBody+maskDefs.swift
+//  Carbunculo
+//
+//  Created by Débora Moraes on 22/05/19.
+//  Copyright © 2019 CarbunculoTeam. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+extension SKPhysicsBody{
+    public struct CategoryBitMask {
+        // move 0 bits para a esquerda
+        public static let player: UInt32 = 1 << 0
+    }
+}
+
