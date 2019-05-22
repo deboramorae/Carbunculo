6916ec1261d63ddf4a3450db2f5e872761fa49ae
Débora Moraes <deboramp.lissa@gmail.com>
2019-05-22 14:50:43 -0400

    Adicionando collision floor e player

--- Developer/Carbunculo/Carbunculo/Components/PhysicsBodyComponent.swift
+++ Developer/Carbunculo/Carbunculo/Components/PhysicsBodyComponent.swift
@@ -55,7 +55,7 @@ class PhysicsBodyComponent: GKComponent {
         node.physicsBody?.friction           = 0.0
         node.physicsBody?.restitution        = 0.0
         
-        node.physicsBody?.categoryBitMask    = 0
+        node.physicsBody?.categoryBitMask    = SKPhysicsBody.CategoryBitMask.floorComponent
         node.physicsBody?.collisionBitMask   = 0
         node.physicsBody?.contactTestBitMask = 0
         
