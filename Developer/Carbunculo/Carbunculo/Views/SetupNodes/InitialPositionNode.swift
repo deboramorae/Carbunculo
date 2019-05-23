3852fac6777f1074886899b3133d1146db100a12
Jefferson Oliveira <jeffersonlalor19@gmail.com>
2019-05-23 16:57:28 -0400

    Criando arquivos para tamanho e posicionamento inicial padrao dos nodes

---
+++ Developer/Carbunculo/Carbunculo/Views/SetupNodes/InitialPositionNode.swift
@@ -0,0 +1,9 @@
+//
+//  InitialPositionNode.swift
+//  Carbunculo
+//
+//  Created by Jefferson de Oliveira Lalor on 23/05/19.
+//  Copyright © 2019 CarbunculoTeam. All rights reserved.
+//
+
+import Foundation
