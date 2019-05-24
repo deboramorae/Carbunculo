5d473b96318efbd6c5522f428697d75b6a47cfb6
Jefferson Oliveira <jeffersonlalor19@gmail.com>
2019-05-24 14:36:48 -0400

    Ajuste no tamanho do node da agua

--- Developer/Carbunculo/Carbunculo/Views/SetupNodes/SizeNode.swift
+++ Developer/Carbunculo/Carbunculo/Views/SetupNodes/SizeNode.swift
@@ -15,7 +15,7 @@ extension CGSize {
         public static let playerNode: CGSize = CGSize(width: 55, height: 125)
         public static let floorNode: CGSize = CGSize(width: 955, height: 43)
         public static let woodNode: CGSize = CGSize(width: 136, height: 43)
-//        public static let waterNode: CGSize = CGSize(width: , height: )
+//        public static let waterNode: CGSize = CGSize(width: 405, height: 23)
         public static let platformNode: CGSize = CGSize(width: 120, height: 40)
         
     }
