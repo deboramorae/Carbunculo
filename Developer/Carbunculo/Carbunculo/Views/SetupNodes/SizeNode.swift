8ae42035842f711956de3dae4f38410c3fc1040f
Jefferson Oliveira <jeffersonlalor19@gmail.com>
2019-05-24 14:33:43 -0400

    Ajuste no tamanho do chao

--- Developer/Carbunculo/Carbunculo/Views/SetupNodes/SizeNode.swift
+++ Developer/Carbunculo/Carbunculo/Views/SetupNodes/SizeNode.swift
@@ -13,7 +13,7 @@ extension CGSize {
     public struct sizeNode {
         
         public static let playerNode: CGSize = CGSize(width: 55, height: 125)
-        public static let floorNode: CGSize = CGSize(width: 800, height: 50)
+        public static let floorNode: CGSize = CGSize(width: 955, height: 43)
         public static let woodNode: CGSize = CGSize(width: 50, height: 20)
 //        public static let waterNode: CGSize = CGSize(width: , height: )
         public static let platformNode: CGSize = CGSize(width: 120, height: 40)
