39cd6ae531cdc12dba4da0341892c8267a345e4f
Jefferson Oliveira <jeffersonlalor19@gmail.com>
2019-05-28 16:16:30 -0400

    Funcao que calcula a proporcao de acordo com tamanho do node de do numero de pixels

--- Developer/Carbunculo/Carbunculo/Extensions/SetupNodes/SizeNode.swift
+++ Developer/Carbunculo/Carbunculo/Extensions/SetupNodes/SizeNode.swift
@@ -12,21 +12,25 @@ import CoreGraphics
 extension CGSize {
     public struct sizeNode {
         
-        public static let backgroundNode: CGSize = CGSize(width: (1480 * SKViewSizeRect.width) / 667, height: (375 * SKViewSizeRect.height) / 375 )
-        public static let playerNode: CGSize = CGSize(width: (55 * SKViewSizeRect.width) / 667, height: (125 * SKViewSizeRect.height) / 375 )
-//        public static let playerNode: CGSize = CGSize(width: (SKViewSizeRect.width * 8.25)/100, height: (SKViewSizeRect.height * 33.34)/100)
+        public static let backgroundNode: CGSize = CGSize.sizeProportinalWithFrame(size: CGSize(width: 1480, height: 375))
+        public static let playerNode: CGSize = CGSize.sizeProportinalWithFrame(size: CGSize(width: 55, height: 125))
+        public static let floorNode: CGSize = CGSize.sizeProportinalWithFrame(size: CGSize(width: 955, height: 43))
+        public static let woodNode: CGSize = CGSize.sizeProportinalWithFrame(size: CGSize(width: 136, height: 43))
+        public static let platformNode : CGSize = CGSize.sizeProportinalWithFrame(size: CGSize(width: 252, height: 44))
 
-        public static let floorNode: CGSize = CGSize(width: 955, height: 43)
-        public static let woodNode: CGSize = CGSize(width: 136, height: 43)
-//        public static let waterNode: CGSize = CGSize(width: 405, height: 23)
-        public static let platformNode : CGSize = CGSize(width: 252, height: 44)
         
         //pegar o tamanho do vento/agua real depois
+
+        public static let windNode    : CGSize  = CGSize.sizeProportinalWithFrame(size: CGSize(width: 200, height: 50))
+        public static let waterNode   : CGSize  = CGSize.sizeProportinalWithFrame(size: CGSize(width: 134, height: 46))
+        public static let animalNode  : CGSize  = CGSize.sizeProportinalWithFrame(size: CGSize(width: 125, height: 55))
+
+    }
+    
+    public static func sizeProportinalWithFrame( size: CGSize ) -> CGSize {
         
-        public static let windNode    : CGSize  = CGSize(width: 200, height: 50)
-        public static let waterNode   : CGSize  = CGSize(width: 134, height: 46)
+        let newSize: CGSize = CGSize(width: (size.width * SKViewSizeRect.width) / 667, height: (size.height * SKViewSizeRect.height) / 375 )
         
-        public static let animalNode  : CGSize  = CGSize(width: 125, height: 55)
-        
+        return newSize
     }
 }
