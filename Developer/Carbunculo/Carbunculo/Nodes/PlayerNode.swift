ce421c55783f38399183ab249d55bf37523b71a1
Jefferson Oliveira <jeffersonlalor19@gmail.com>
2019-05-28 23:13:27 -0400

    Ajuste no tamanho do corpo físico

--- Developer/Carbunculo/Carbunculo/Nodes/PlayerNode.swift
+++ Developer/Carbunculo/Carbunculo/Nodes/PlayerNode.swift
@@ -15,6 +15,7 @@ class PlayerNode: SKSpriteNode {
         super.init(texture: SKTexture(imageNamed: "akin-1"), color: .blue, size: CGSize.sizeNode.playerNode)
         self.name = "Player"
         self.initialPosition()
+        self.zPosition = 1
     }
 
     private func initialPosition(){
