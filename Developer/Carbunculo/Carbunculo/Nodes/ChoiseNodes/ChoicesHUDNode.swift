b2586a93ffc201837a5f63df04d1ca16b2df0bb0
Jefferson Oliveira <jeffersonlalor19@gmail.com>
2019-05-30 09:46:09 -0400

    Funcao que adiciona a pontuacao da escolha do usuario

--- Developer/Carbunculo/Carbunculo/Nodes/ChoiseNodes/ChoicesHUDNode.swift
+++ Developer/Carbunculo/Carbunculo/Nodes/ChoiseNodes/ChoicesHUDNode.swift
@@ -20,6 +20,8 @@ class ChoicesHUDNode : SKSpriteNode {
         let choiseButton1 = ChoiceButtonNode(backgroundNamed: "botaoPause") {
             print("Botão 1 pressionado")
             self.removeChoicesHUD()
+            self.chooseWithMorePoints()
+            print(choicesControl.ponctuation)
         }
         choiseButton1.position = CGPoint.positionProportionalWithFrame(position: CGPoint(x: 0, y: -30))
         self.addChild(choiseButton1)
@@ -27,10 +29,20 @@ class ChoicesHUDNode : SKSpriteNode {
         let choiseButton2 = ChoiceButtonNode(backgroundNamed: "botaoPause") {
             print("Botão 2 pressionado")
             self.removeChoicesHUD()
+            self.chooseWithFewerPoints()
+            print(choicesControl.ponctuation)
         }
         choiseButton2.position = CGPoint.positionProportionalWithFrame(position: CGPoint(x: 0, y: -95))
         self.addChild(choiseButton2)
         
+    }
+    
+    private func chooseWithMorePoints() {
+        choicesControl.ponctuation += 2
+    }
+    
+    private func chooseWithFewerPoints() {
+        choicesControl.ponctuation += 1
     }
     
     private func removeChoicesHUD() {
