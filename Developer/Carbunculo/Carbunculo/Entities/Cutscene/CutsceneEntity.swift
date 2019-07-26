25061abefada9a4cb3eee49b16a33ddda2065fec
Débora Moraes <deboramp.lissa@gmail.com>
2019-07-26 02:16:27 -0400

    Adicionado button skip

--- Developer/Carbunculo/Carbunculo/Entities/Cutscene/CutsceneEntity.swift
+++ Developer/Carbunculo/Carbunculo/Entities/Cutscene/CutsceneEntity.swift
@@ -18,17 +18,56 @@ class CutsceneEntity: GKEntity {
         
         let player = AVPlayer(url: URL(fileURLWithPath: path!))
         player.actionAtItemEnd = .none
-
+        
+        
+        
+        
         let cutsceneNode = CutsceneNode(player: player)
         cutsceneNode.name = "cutsceneNode"
         cutsceneNode.position = CGPoint(x: 0, y: 0)
         cutsceneNode.size = CGSize(width: scene.frame.size.width, height: scene.frame.size.height)
         cutsceneNode.zPosition = 100
         cutsceneNode.play()
-
+        
+        if nameFile == "cutsceneInicio"{
+            
+            let buttonSkip = ButtonSkipNode(texture: SKTexture(imageNamed: "pular")){
+                
+                guard let durationTime = player.currentItem?.duration else { return }
+                
+                
+                let percentageTime = CMTimeMultiplyByFloat64(durationTime, multiplier: 100.2)
+                
+                guard percentageTime.isValid && percentageTime.isNumeric else { return }
+                
+                var targetTime = player.currentTime() + percentageTime
+                targetTime = targetTime.convertScale(durationTime.timescale, method: .default)
+                
+                guard targetTime.isValid && targetTime.isNumeric else { return }
+                
+                if targetTime > durationTime {
+                    targetTime = durationTime
+                }
+                
+                player.seek(to: targetTime)
+                entityManager.removeButtonSkip()
+            }
+            
+            buttonSkip.position = CGPoint(x: 280, y: -150)
+            buttonSkip.zPosition = 101
+            
+            addComponent(buttonSkipComponent(node: buttonSkip, scene: scene))
+        }
+        
+        
+        
+        
         NotificationCenter.default.addObserver(self, selector: #selector(self.test(notitication:)), name: NSNotification.Name.AVPlayerItemDidPlayToEndTime, object: player.currentItem)
+        
 
         addComponent(CutsceneNodeComponent(node: cutsceneNode, scene: scene))
+        
+        
     }
     
     @objc func test(notitication: NSNotification) {
@@ -38,4 +77,6 @@ class CutsceneEntity: GKEntity {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+   
 }
